55a5f246486a56c54e341ec056379a4ff0e98cf4
Adrian Thomas <adrian.l.thomas@outlook.com>
2017-12-10 19:18:32 -0600

    hooks up the user account creation page as well as create a view for the Login/Create acct btn

--- Smack/Controller/CreateAccountVC.swift
+++ Smack/Controller/CreateAccountVC.swift
@@ -9,15 +9,38 @@
 import UIKit
 
 class CreateAccountVC: UIViewController {
-
-    override func viewDidLoad() {
+    // Outlets
+  @IBOutlet weak var usernameTxt: UITextField!
+  @IBOutlet weak var emailTxt: UITextField!
+  @IBOutlet weak var passwdTxt: UITextField!
+  
+  @IBOutlet weak var usrImg: UIImageView!
+  
+  override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-
+  
+  // Go back to rooms screen once you close from the Create account VC
   @IBAction func closePressed(_ sender: Any) {
     performSegue(withIdentifier: UNWIND, sender: nil)
   }
+  
+  @IBAction func createAcctPressed(_ sender: Any) {
+    guard let email = emailTxt.text, emailTxt.text != "" else { return }
+    guard let password = passwdTxt.text, passwdTxt.text != "" else { return }
+    
+    AuthService.instance.registerUser(email: email, password: password) { (success) in
+      if success {
+        print("registered user!")
+      }
+    }
+  }
+  @IBAction func pickAvatarPressed(_ sender: Any) {
+  }
+  @IBAction func pickBGColorPressed(_ sender: Any) {
+  }
+  
   
 }
